b94c5cd7281236e1d52376bd26730f15e503a6c5
MabdoulYoussef <mabdoulyoussef@hotmail.com>
2025-08-16 14:51:10 +0100

    Update Front Pages with Improved Arabic Styling and Structure
    Prepared the structure for future product pages integration with Laravel backend and database (seeders planned for dynamic product population).

--- ecom-backend/resources/views/home/about.blade.php
+++ ecom-backend/resources/views/home/about.blade.php
@@ -1,23 +1,31 @@
 @extends( 'Layouts.master')
 
-@section( 'content')
+@section('content')
+    <!-- hero area -->
+    <div class="hero-area hero-bg">
+        <div class="container">
+            <div class="row align-items-center">
+                <!-- Text -->
+                <div class="col-lg-6 text-center">
+                    <div class="aboutus-text"  >
+                        <h1>  من نحن</h1><br><br>
+                        <p class="aboutus-p">منذ سنوات، كان حلمي الأكبر أن أصبح أستاذًا في الجامعة، أعيش بين قاعات المحاضرات وأشارك المعرفة مع الآخرين. لكن مع مرور الوقت، اكتشفت أن بداخلي شغفًا مختلفًا، شغفًا يدفعني إلى الإبداع وإلى بناء شيء يعكس هويتي وثقافتي المغربية. <br><br>
 
+في عام 2018، بدأت مساري الأكاديمي بدراسة عالم الأزياء والاقتصاد، وكان ذلك الباب الذي فتح أمامي أفقًا جديدًا. لم تكن الدراسة مجرد واجب، بل كانت وسيلتي لفهم كيف يمكن للشغف أن يتحوّل إلى مشروع حيّ<br><br>
 
-	<div class="hero-area hero-bg">
-		<div class="container">
-			<div class="row align-items-center">
-				<!-- Text -->
-				<div class="col-lg-6">
-					<div class="hero-text-tablecell">
+وفي عام 2019، جاءت الخطوة الحاسمة: ولادة Moda2Shop، علامة مغربية خالصة، تحمل في طياتها هوية بلدي، وتطمح لأن تنافس عالميًا. كانت تلك البداية بمثابة مخاطرة، لكنها في الحقيقة كانت الشرارة الأولى لحلمٍ أكبر.<br><br>
+اليوم لم يعد النجاح الشخصي غايتي الوحيدة، بل أحمل رسالة أسمى: أن أثبت أنّ الشغف إذا امتزج بالعمل الجاد قادر على تحويل الحلم إلى حقيقة، وأن الهوية المغربية جديرة بأن تتجلّى في أبهى صورها عبر الموضة والإبداع. <br><br></p>
+                    </div>
+                </div>
 
-    @yield('content')
-
-					</div>
-				</div>
-
-				<!-- Image -->
-
-			</div>
-		</div>
-	</div>
+                <!-- Image -->
+                <div class="col-lg-6 text-center">
+                    <div class="img-fluid" style="margin-bottom: 2rem;" data-aos="fade-up" data-aos-delay="200">
+                        <img src="assets/img/img-beach.JPG" alt="About Us Image" class="img-fluid">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end hero area -->
 @endsection
